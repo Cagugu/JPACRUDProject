c692589f70816e1363f59117ad0d91dde023c7f8
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-04-15 20:30:58 -0500

    Start Boot MVC project and add JPA dependancies

---
+++ ColumbineCoffeHouse/src/main/java/com/skilldistillery/coffee/ColumbineCoffeHouseApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbineCoffeHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumbineCoffeHouseApplication.class, args);
+	}
+
+}
